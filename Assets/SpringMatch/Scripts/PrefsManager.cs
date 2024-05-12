c58f3de34af871eb07dec16a9b6db25fbf0436b0
infinityww88 <138868906+infinityww88@users.noreply.github.com>
2024-05-12 11:47:29 +0800

    Add UI elements.

---
+++ Assets/SpringMatch/Scripts/PrefsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpringMatch {
+	
+	public static class PrefsManager
+	{
+		public const string GOLD = "gold";
+		public const string HEART = "heart";
+		public const string REVOKE_ITEM = "revoke_item";
+		public const string SHIFT_ITEM = "shift_item";
+		public const string RANDOM_ITEM = "random_item";
+	
+		public static int GetInt(string key, int dafaultValue) {
+			return PlayerPrefs.GetInt(key, dafaultValue);
+		}
+	
+		public static void SetInt(string key, int value) {
+			PlayerPrefs.SetInt(key, value);
+		}
+	}
+
+}
---
+++ Assets/SpringMatch/Scripts/UI/ItemButton.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using SpringMatch;
+
+namespace SpringMatch.UI {
+	
+	public class ItemButton : MonoBehaviour
+	{
+		[SerializeField]
+		private Sprite _grayBg, _normalBg;
+		
+		[SerializeField]
+		private TextMeshProUGUI _itemNumText;
+		
+		private int _itemNum;
+		private bool _valid = false;
+		
+		[SerializeField]
+		private string _saveKey;
+		
+		[Button]
+		public int ItemNum {
+			get {
+				return _itemNum;
+			}
+			set {
+				_itemNum = value;
+				_itemNumText.text = $"{_itemNum}";
+				PrefsManager.SetInt(_saveKey, value);
+			}
+		}
+		
+		[Button]
+		public bool Valid {
+			get {
+				return _valid;
+			}
+			set {
+				GetComponent<Image>().sprite = value ? _normalBg : _grayBg;
+				_valid = value;
+			}
+		}
+		
+		public void OnClick() {
+			if (ItemNum > 0) {
+				Debug.Log($"Use a item");
+				ItemNum--;
+			} else {
+				UIVariable.Inst.itemRequestDialog.gameObject.SetActive(true);
+			}
+		}
+		
+		// Awake is called when the script instance is being loaded.
+		protected void Awake()
+		{
+			_itemNum = PrefsManager.GetInt(_saveKey, 0);
+			Debug.Log(_itemNum);
+			_itemNumText.text = $"{_itemNum}";
+		}
+	}
+}
+
