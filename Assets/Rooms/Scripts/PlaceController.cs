d54f3ec2fbb2b812350811ffd51ddf343b7e6bc2
infinityww88 <138868906+infinityww88@users.noreply.github.com>
2024-07-07 22:02:08 +0800

    Place Controller.

--- Assets/Rooms/Scripts/PlaceController.cs
+++ Assets/Rooms/Scripts/PlaceController.cs
@@ -5,25 +5,96 @@ using Sirenix.OdinInspector;
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using System.Linq;
+using ScriptableObjectArchitecture;
 
 namespace CustomRoom {
 	
 	public class PlaceController : MonoBehaviour
 	{
 		[SerializeField]
-		private float offsetScale = 0.1f;
-		[SerializeField]
 		private LayerMask groundMask;
 		[SerializeField]
 		private Color pickupColor;
 		[SerializeField]
 		private LayerMask castLayer;
+		[SerializeField]
+		private FloatVariable rotateFactor, moveFactor, heightFactor;
+		
+		private CancellationTokenSource cancelTokenSource = null;
 		
 		// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
 		protected void Start()
 		{
-			CancellationToken token = gameObject.GetCancellationTokenOnDestroy();
-			DragHandler(token).Forget();
+			
+		}
+		
+		public void StartHorzEdit() {
+			if (cancelTokenSource != null) {
+				cancelTokenSource.Cancel();
+				cancelTokenSource = null;
+			}
+			cancelTokenSource = new CancellationTokenSource();
+			EditHandler(cancelTokenSource.Token, HorzHandler).Forget();
+		}
+		
+		public void StartRotateEdit() {
+			if (cancelTokenSource != null) {
+				cancelTokenSource.Cancel();
+				cancelTokenSource = null;
+			}
+			cancelTokenSource = new CancellationTokenSource();
+			EditHandler(cancelTokenSource.Token, RotateHandler).Forget();
+		}
+		
+		public void StartHeightEdit() {
+			if (cancelTokenSource != null) {
+				cancelTokenSource.Cancel();
+				cancelTokenSource = null;
+			}
+			cancelTokenSource = new CancellationTokenSource();
+			EditHandler(cancelTokenSource.Token, HeightHandler).Forget();
+		}
+		
+		[Button]
+		private void Test(float angle) {
+			Debug.Log($"{Mathf.Repeat(angle, 360)}");
+		}
+		
+		public void Rotate90Clockwise() {
+			if (lastPickupObj == null) {
+				return;
+			}
+			Snap90Degree(lastPickupObj.transform);
+			lastPickupObj.transform.eulerAngles -= Vector3.up * 90;
+		}
+		
+		public void Rotate90AntiClockwise() {
+			if (lastPickupObj == null) {
+				return;
+			}
+			Snap90Degree(lastPickupObj.transform);
+			lastPickupObj.transform.eulerAngles += Vector3.up * 90;
+		}
+		
+		public void Snap90Degree(Transform t) {
+			var y = t.eulerAngles.y;
+			y = Mathf.Repeat(y, 360);
+			for (int i = 0; i < 5; i++) {
+				float a = i * 90;
+				if (Mathf.Abs(y - a) <= 45) {
+					t.eulerAngles = new	Vector3(0, a, 0);
+					break;
+				}
+			}
+		}
+		
+		// This function is called when the MonoBehaviour will be destroyed.
+		protected void OnDestroy()
+		{
+			if (cancelTokenSource != null) {
+				cancelTokenSource.Cancel();
+			}
+			cancelTokenSource = null;
 		}
 		
 		// Update is called every frame, if the MonoBehaviour is enabled.
@@ -44,14 +115,14 @@ namespace CustomRoom {
 				Outline outline = o.GetComponentInParent<Outline>();
 				outline.enabled = true;
 				outline.OutlineColor = pickupColor;
-				if (lastPickupObj != null) {
+				if (lastPickupObj != null && lastPickupObj != outline.gameObject) {
 					lastPickupObj.GetComponent<Outline>().enabled = false;
 				}
 				lastPickupObj = outline.gameObject;
 			}
 		}
 		
-		private async UniTaskVoid DragHandler(CancellationToken token) {
+		private async UniTaskVoid EditHandler(CancellationToken token, System.Action<Vector2, Vector2> func) {
 			while (!token.IsCancellationRequested) {
 				while (!Input.GetMouseButtonDown(0)) {
 					await UniTask.NextFrame();
@@ -65,16 +136,37 @@ namespace CustomRoom {
 					if (token.IsCancellationRequested) {
 						return;
 					}
-					Ray r0 = Camera.main.ScreenPointToRay(lastPos);
-					Ray r1 = Camera.main.ScreenPointToRay(Input.mousePosition);
-					Physics.Raycast(r0, out RaycastHit h0, Mathf.Infinity, groundMask);
-					Physics.Raycast(r1, out RaycastHit h1, Mathf.Infinity, groundMask);
-					Vector3 diff = h1.point - h0.point;
-					diff = diff.normalized * offsetScale * (Input.mousePosition - lastPos).magnitude;
-					transform.Translate(diff, Space.World);
+					func(Input.mousePosition, lastPos);
 					lastPos = Input.mousePosition;
 				}
 			}
+		}
+		
+		private void HorzHandler(Vector2 currentMousePos, Vector2 lastMousePos) {
+			Ray r0 = Camera.main.ScreenPointToRay(lastMousePos);
+			Ray r1 = Camera.main.ScreenPointToRay(currentMousePos);
+			Physics.Raycast(r0, out RaycastHit h0, Mathf.Infinity, groundMask);
+			Physics.Raycast(r1, out RaycastHit h1, Mathf.Infinity, groundMask);
+			Vector3 diff = (h1.point - h0.point) * moveFactor.Value;
+			if (lastPickupObj != null) {
+				lastPickupObj.transform.Translate(diff, Space.World);
+			}
+		}
+		
+		private void RotateHandler(Vector2 currentMousePos, Vector2 lastMousePos) {
+			if (lastPickupObj == null) {
+				return;
+			}
+			Vector2 diff = currentMousePos - lastMousePos;
+			lastPickupObj.transform.Rotate(0, diff.x * rotateFactor.Value, 0, Space.Self);
+		}
+		
+		private void HeightHandler(Vector2 currentMousePos, Vector2 lastMousePos) {
+			if (lastPickupObj == null) {
+				return;
+			}
+			Vector2 diff = currentMousePos - lastMousePos;
+			lastPickupObj.transform.Translate(new Vector3(0, diff.y * heightFactor.Value, 0), Space.Self);
 		}
 	}
 
